353e02ef41de6cfd0572593f2119c1f6c538811a
Matheus Monteiro <matheus.stack.20@hotmail.com>
2022-10-27 08:39:43 -0300

    feat: Desafio Teste unitario

--- src/modules/statements/useCases/createTransfer/CreateTransferController.ts
+++ src/modules/statements/useCases/createTransfer/CreateTransferController.ts
@@ -1,30 +1,30 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
-import { CreateStatementUseCase } from './CreateStatementUseCase';
+import { CreateTransferUseCase } from './CreateTransferUseCase';
 
 enum OperationType {
-  DEPOSIT = 'deposit',
-  WITHDRAW = 'withdraw',
+  TRANSFER = 'transfer',
 }
 
-export class CreateStatementController {
+export class CreateTransferController {
   async execute(request: Request, response: Response) {
     const { id: user_id } = request.user;
-    const { amount, description } = request.body;
+    const { amount, description, sender_id } = request.body;
 
     const splittedPath = request.originalUrl.split('/')
     const type = splittedPath[splittedPath.length - 1] as OperationType;
 
-    const createStatement = container.resolve(CreateStatementUseCase);
+    const createTransfer = container.resolve(CreateTransferUseCase);
 
-    const statement = await createStatement.execute({
+    const statement = await createTransfer.execute({
       user_id,
+      sender_id,
       type,
       amount,
       description
     });
 
-    return response.status(201).json(statement);
+    return response.status(201).send(statement);
   }
 }
